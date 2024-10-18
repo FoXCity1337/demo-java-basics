32d19a19d24f5e0d377fb8798960a27f00fd328b
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-19 00:56:07 +0300

    Создание метода getStringFromInt

--- src/by/itacademy/arsenialeksandrovich/numbers/Main.java
+++ src/by/itacademy/arsenialeksandrovich/numbers/Main.java
@@ -1,5 +1,8 @@
 package by.itacademy.arsenialeksandrovich.numbers;
 
 public class Main {
+    int number = 1;
+    String numberAsAString = "";
+
 
 }
--- src/by/itacademy/arsenialeksandrovich/numbers/Utils.java
+++ src/by/itacademy/arsenialeksandrovich/numbers/Utils.java
@@ -1,5 +1,18 @@
 package by.itacademy.arsenialeksandrovich.numbers;
 
 public class Utils {
-
+    public String getStringFromInt(int num) {
+        return switch (num) {
+            case 1 -> "один";
+            case 2 -> "два";
+            case 3 -> "три";
+            case 4 -> "четыре";
+            case 5 -> "пять";
+            case 6 -> "шесть";
+            case 7 -> "семь";
+            case 8 -> "восемь";
+            case 9 -> "девять";
+            default -> "";
+        };
+    }
 }
