c04ab0ae1243e72571e8ad732c8e795c2fba91d0
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-19 23:47:42 +0300

    Создание полей title, HOME_URL и их геттеров/сеттеров

--- src/by/itacademy/arsenialeksandrovich/pages/HomePage.java
+++ src/by/itacademy/arsenialeksandrovich/pages/HomePage.java
@@ -1,5 +1,22 @@
 package by.itacademy.arsenialeksandrovich.pages;
 
 public class HomePage extends BasePage {
+    protected String title = "Домашняя страница";
+    protected String HOME_URL = getBASE_URL() + "/" + getTitle();
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getHOME_URL() {
+        return HOME_URL;
+    }
+
+    public void setHOME_URL(String HOME_URL) {
+        this.HOME_URL = HOME_URL;
+    }
 }
