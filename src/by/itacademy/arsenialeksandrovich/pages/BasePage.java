2c5190c82b70dc0147f65a2e39c88814eac65730
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-19 23:36:19 +0300

    Инициализация поля BASE_URL и его геттера/сеттера. Создание метода open().

--- src/by/itacademy/arsenialeksandrovich/pages/BasePage.java
+++ src/by/itacademy/arsenialeksandrovich/pages/BasePage.java
@@ -1,4 +1,18 @@
 package by.itacademy.arsenialeksandrovich.pages;
 
 public class BasePage {
+    protected String BASE_URL = "BASE_URL";
+
+    public String getBASE_URL() {
+        return BASE_URL;
+    }
+
+    public void setBASE_URL(String BASE_URL) {
+        this.BASE_URL = BASE_URL;
+    }
+
+    public void open() {
+        System.out.println("Пользователь открыл страницу " + getBASE_URL());
+    }
+
 }
