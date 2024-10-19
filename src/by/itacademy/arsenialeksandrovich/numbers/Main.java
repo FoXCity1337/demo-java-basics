ecfa288ce28bfc55edaf8f1152eae9a2c82cc0a3
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-19 23:08:14 +0300

    Изменил формат оператора Switch

--- src/by/itacademy/arsenialeksandrovich/numbers/Main.java
+++ src/by/itacademy/arsenialeksandrovich/numbers/Main.java
@@ -2,11 +2,8 @@ package by.itacademy.arsenialeksandrovich.numbers;
 
 public class Main {
     public static void main(String[] args) {
-        int number;
-        String numberAsAString;
         for (int i = 1; i < 10; i++) {
-            number = i;
-            numberAsAString = Utils.getStringFromInt(number);
+            String numberAsAString = Utils.getStringFromInt(i);
             System.out.println(numberAsAString);
         }
     }
