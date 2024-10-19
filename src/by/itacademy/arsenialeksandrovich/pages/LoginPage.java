51b0aa62c5bbc1baa0618b57546afad784301eed
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-20 00:03:30 +0300

    Создание полей title и LOGIN_PAGE

--- src/by/itacademy/arsenialeksandrovich/pages/LoginPage.java
+++ src/by/itacademy/arsenialeksandrovich/pages/LoginPage.java
@@ -1,5 +1,14 @@
 package by.itacademy.arsenialeksandrovich.pages;
 
-public class LoginPage extends BasePage{
+public class LoginPage extends BasePage {
+    String title = "Форма логина";
+    protected String LOGIN_PAGE = getBASE_URL() + "/" + getTitle();
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
